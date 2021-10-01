1f729c2de536ca8cef6230b0881a0674e3a1cd80
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-10-01 23:16:25 +0200

    feat: 🎸 the most useless app in the hello world

---
+++ apps/setgo/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <h1>App!</h1>
+    </>
+  );
+};
--- apps/setgo/src/index.ts
+++
@@ -1,4 +0,0 @@
-const _fOO: string = 'Foo!1234';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const x: string = _fOO;
